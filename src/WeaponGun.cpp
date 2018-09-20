34fa1b6ef335651bf411b05347172a6918acc589
EduardoBrasil <brasil.eduardo1@gmail.com>
2018-09-20 18:05:11 -0300

    adding some more coments at weapon gun and change some variables

--- src/WeaponGun.cpp
+++ src/WeaponGun.cpp
@@ -1,7 +1,7 @@
 /*****************************************************
-	* File: WeaponGun.cpp
-	* Purpose: Implementation of class WeaponGun
-******************************************************/
+ * File: WeaponGun.cpp
+ * Purpose: Implementation of class WeaponGun
+ ******************************************************/
 #include "WeaponGun.h"
 
 #include <SDL_keycode.h>
@@ -11,9 +11,14 @@
 #include "InputManager.h"
 #include "Point.h"
 
-WeaponGun::WeaponGun(std::string file) : sprite(file,0.1,2,8)
-{
-	isDead = false;
+#define QUARTER_OF_A_TURN 90
+
+WeaponGun::WeaponGun(std::string file) :
+		sprite(file, 0.1, 2, 8) {
+
+	// Set the weapon gun position on sprite and get position on the box
+
+	isDead = false; //make sure the player is not dead
 	sprite.SetScaleX(0.5);
 	sprite.SetScaleY(0.5);
 	box.setX(Player::player->box.x);
@@ -24,67 +29,67 @@ WeaponGun::WeaponGun(std::string file) : sprite(file,0.1,2,8)
 	atkFrame = false;
 
 	/*
-	 * Checking players orientation and if this orientation
-	 * was in left direction flip is ocurred
-	*/
+	 * Checking players orientation
+	 * to turn weapon gun in he's direction
+	 */
 	orientation = Player::player->orientation;
 	if (orientation == LEFT) {
 		sprite.SetFlipH(true);
 	}
 }
 
-WeaponGun::~WeaponGun()
-{
+WeaponGun::~WeaponGun() {
 
 }
 
-/*
- * update position, using position and time to realize ajust
- * with data a previous move
- * */
-void WeaponGun::Update(float deltaTime)
-{
-	if (Player::player->GetHp() <= 0)
-	{
+void WeaponGun::Update(float deltaTime) {
+
+	// check if Player still have HP to update to sprite
+	if (Player::player->GetHp() <= 0) {
 		somaRotation = 0;
-		sprite.SetLoop(10,10);
+		int loopStart = 10;
+		int loopEnd = 10;
+		sprite.SetLoop(loopStart, loopEnd);
 		sprite.Update(1);
 		return;
 	}
 
 	deathCD.Update(deltaTime);
 
-	if (Player::player == NULL)
+	// verify player status to adjust and to stop akt
+	if (Player::player == NULL) {
 		return;
+	}
 
 	box.setX(Player::player->box.x);
 	box.setY(Player::player->box.y);
 	orientation = Player::player->orientation;
 
-	if (orientation == LEFT)
+	if (orientation == LEFT) {
 		sprite.SetFlipH(true);
-	else
+	} else {
 		sprite.SetFlipH(false);
-
-	if (sprite.GetCurrentFrame() == 9)
+	}
+	if (sprite.GetCurrentFrame() == 9){
 		atkFrame = false;
+	}
 
-	if(InputManager::GetInstance().KeyPress(SDLK_a) || atkFrame)
-	{
+
+	// Use the keyboard to detect the attack and start sound atack
+	// passing the initial and final Loop parameters to update the sprite
+
+	if (InputManager::GetInstance().KeyPress(SDLK_a) || atkFrame) {
 		Sound* sound = new Sound("audio/projetil.wav");
 		sound->Play(0);
-		delete(sound);
+		delete (sound);
 		atkFrame = true;
-		sprite.SetLoop(8, 9);
-	}
-	else
-	{
-		if (Player::player->loopStart == 16 && Player::player->loopEnd == 17)
-		{
-			sprite.SetLoop(0,0);
-		}
-		else
-		{
+		int loopStart = 8;
+		int loopEnd = 9;
+		sprite.SetLoop(8,9);
+	} else {
+		if (Player::player->loopStart == 16 && Player::player->loopEnd == 17) {
+			sprite.SetLoop(0, 0);
+		} else {
 			sprite.SetLoop(0, 7);
 			if (Player::player->planet->somaRotation == 0)
 				sprite.SetLoop(2, 2);
@@ -92,46 +97,41 @@ void WeaponGun::Update(float deltaTime)
 	}
 
 	if (orientation == RIGHT)
-		rotation += 90;
+		rotation += QUARTER_OF_A_TURN;
 	else
-		rotation -= 90;
+		rotation -= QUARTER_OF_A_TURN;
 
 	sprite.Update(deltaTime);
 
-	if(InputManager::GetInstance().KeyPress(SDLK_d) && deathCD.Get() > 0.5)
-	{
+	if (InputManager::GetInstance().KeyPress(SDLK_d) && deathCD.Get() > 0.5) {
 		isDead = true;
 	}
 }
 
-void WeaponGun::Render()
-{
-	int c = 0;
+void WeaponGun::Render() {
+	int center = 0;
 
 	if (orientation == RIGHT)
-		c = -25;
+		center = -25;
 	else
-		c = 25;
+		center = 25;
 
-	sprite.Render(box.getX() +  Camera::pos.getX() + c,box.getY() +  Camera::pos.getY());
+	sprite.Render(box.getX() + Camera::pos.getX() + center,
+			box.getY() + Camera::pos.getY());
 }
 
-bool WeaponGun::IsDead()
-{
+bool WeaponGun::IsDead() {
 	return ((Player::player == NULL) || isDead);
 }
 
-Sprite WeaponGun::getSprite()
-{
+Sprite WeaponGun::getSprite() {
 	return sprite;
 }
 
-bool WeaponGun::Is(string type)
-{
+bool WeaponGun::Is(string type) {
 	return (type == "WeaponGun");
 }
 
-void WeaponGun::NotifyCollision(GameObject&)
-{
+void WeaponGun::NotifyCollision(GameObject&) {
 
 }
